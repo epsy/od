9f2b24ddb8660d47c8febfdf8f900c98ad75805b
Yann Kaiser <y.kaiser@criteo.com>
2022-01-23 19:50:41 -0800

    Release 2.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='od',
-    version='1.0',
+    version='2.0.0',
     description='Shorthand syntax for building OrderedDicts',
     license='MIT',
     url='https://github.com/epsy/od',
